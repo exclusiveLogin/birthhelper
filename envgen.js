5bfb5eeb7d5150cd42a3b0db16f85a10128da201
ssavin <ssavin@solit-clouds.ru>
2020-06-04 23:57:28 +0400

    env gen mod

--- envgen.js
+++ envgen.js
@@ -4,10 +4,10 @@ let envPath = 'env.js';
 if(!!fs.existsSync(envPath)){
     let env = {};
 
-    env['baseUrl'] = process.env['BASE_URL'];
-    env['fileServer'] = process.env['FILE_URL'];
-    env['backend'] = process.env['BACK_URL'];
-    env['static'] = process.env['STATIC_URL'];
+    env['baseUrl'] = process.env['BASE_URL'] || null;
+    env['fileServer'] = process.env['FILE_URL'] || null;
+    env['backend'] = process.env['BACK_URL'] || null;
+    env['static'] = process.env['STATIC_URL'] || null;
 
     fs.writeFileSync(envPath, 'module.exports = ' + JSON.stringify(env, null, 4), 'utf8');
 
