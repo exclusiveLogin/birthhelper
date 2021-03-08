b1fb85bf0e22d08518e4094fe85cdcdc06c38f14
exclucivelogin <alter4444@gmail.com>
2021-03-08 23:42:09 +0400

    ng6 updated

--- src/app/Admin/table/provider.service.ts
+++ src/app/Admin/table/provider.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { DictService, IDictItem } from '../dict.service';
-import {EntityService, IEntity, IEntityItem, ISet} from '../entity.service';
+import {EntityService, IEntityItem, ISet} from '../entity.service';
 import { Observable ,  of } from 'rxjs';
 import { ITableFilter } from './table/table.component';
 import { IRestParams } from '../rest.service';
@@ -11,23 +11,23 @@ export class ProviderService {
   constructor( private dict: DictService, private entity: EntityService ) { }
 
 
-  public getItemsFirstPortion( key: string, type: string ): Observable<IEntityItem[] | IDictItem[]>{
+  public getItemsFirstPortion( key: string, type: string ): Observable<IEntityItem[] | IDictItem[]> {
     return type === 'dict' ? this.getDictPage( key ) : this.getEntPage( key );
   }
 
-  public getItemPage( key: string, type: string, page: number = 1, queryParams: IRestParams = {}): Observable<IEntityItem[] | IDictItem[]>{
+  public getItemPage( key: string, type: string, page: number = 1, queryParams: IRestParams = {}): Observable<IEntityItem[] | IDictItem[]> {
     return type === 'dict' ? this.getDictPage( key, page ) : this.getEntPage( key, page, queryParams );
   }
 
-  public getFilters( key: string, type: string ): Observable<ITableFilter[]>{
+  public getFilters( key: string, type: string ): Observable<ITableFilter[]> {
     return type === 'dict' ? this.getDictFilters( key ) : this.getEntityFilters( key );
   }
 
-  public getItemsSet( key: string, type: string ){
+  public getItemsSet( key: string, type: string ) {
     return ( type ) ? this.getEntSet( key ) : of(null);
   }
 
-  public getDictPage( key: string, page: number = 1){
+  public getDictPage( key: string, page: number = 1) {
     return this.dict.getDict( key, page );
   }
 
@@ -35,19 +35,19 @@ export class ProviderService {
     return this.dict.getDict( key );
   }
 
-  private getEntPage( key: string, page: number = 1, qp?: IRestParams){
+  private getEntPage( key: string, page: number = 1, qp?: IRestParams) {
     return this.entity.getEnt( key, page, qp );
   }
 
-  private getEntSet( key: string ): Observable<ISet>{
+  private getEntSet( key: string ): Observable<ISet> {
     return this.entity.getEntSet( key );
   }
 
-  private getDictFilters( key: string ): Observable<ITableFilter[]>{
-    return Observable.of(null);
+  private getDictFilters( key: string ): Observable<ITableFilter[]> {
+    return of(null);
   }
 
-  private getEntityFilters( key: string ): Observable<ITableFilter[]>{
+  private getEntityFilters( key: string ): Observable<ITableFilter[]> {
     return this.entity.getEntFilters( key );
   }
 
