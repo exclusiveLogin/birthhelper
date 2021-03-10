68343037e92b8b13879a0ca062dd2cb1151b8283
exclucivelogin <alter4444@gmail.com>
2021-03-10 11:33:04 +0400

    api fix

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -6,7 +6,7 @@ import * as env from '../../env.js';
 
 export const environment = {
     production: false,
-    baseUrl: env['baseUrl'] || 'http://birthhelper.ru',
+    baseUrl: env['baseUrl'] || 'http://birthhelper.ru/api',
     fileServer: env['fileServer'] || 'http://birthhelper.ru:9080',
     backend: env['backend'] || 'http://birthhelper.ru:3000',
     static: env['static'] || 'http://birthhelper.ru:3000/static',
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import * as env from '../../env.js';
 
 export const environment = {
   production: true,
-  baseUrl: env['baseUrl'] || 'http://birthhelper.ru',
+  baseUrl: env['baseUrl'] || 'http://birthhelper.ru/api',
   fileServer: env['fileServer'] || 'http://birthhelper.ru:9080',
   backend: env['backend'] || 'http://birthhelper.ru:3000',
   static: env['static'] || 'http://birthhelper.ru:3000/static',
