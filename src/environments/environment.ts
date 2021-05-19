2af3a93ded6dc25adf685f36b87b466a62696144
Sergei Savin <alter4444@gmail.com>
2021-05-19 14:00:20 +0400

    create script start:localhost fix env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ import * as env from '../../env.js';
 
 export const environment = {
   production: false,
-  baseUrl: env['baseUrl'] || 'http://localhost:4200/api',
+  baseUrl: env['baseUrl'] || 'http://localhost:3000',
   fileServer: env['fileServer'] || 'http://localhost:9080',
   backend: env['backend'] || 'http://localhost:3000',
-  static: env['static'] || 'http://localhost:4200/static',
+  static: env['static'] || 'http://localhost:3000/static',
 };
