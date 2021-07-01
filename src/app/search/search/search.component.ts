6f2682009aaff8002caec110bd42e8138ffd9073
exclucivelogin <alter4444@gmail.com>
2021-07-02 00:05:51 +0400

    map mode improved

--- src/app/search/search/search.component.ts
+++ src/app/search/search/search.component.ts
@@ -4,30 +4,39 @@ import {switchMap, tap} from 'rxjs/operators';
 import {DataProviderService} from 'app/services/data-provider.service';
 
 @Component({
-    selector: 'app-search',
-    templateUrl: './search.component.html',
-    styleUrls: ['./search.component.scss']
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit, AfterViewInit {
 
-    onInit$ = new Subject<null>();
-    onFilterChange$ = new Subject<null>();
-    onPageChange$ = new Subject<null>();
-    mainList$ = merge(this.onInit$, this.onFilterChange$, this.onPageChange$).pipe(
-        tap(() => console.log('onInit$')),
-        switchMap(() => this.dataProvider$(1))
-    );
-    dataProvider$ = this.provider.getListProvider('clinics');
+  onInit$ = new Subject<null>();
+  onFilterChange$ = new Subject<null>();
+  onPageChange$ = new Subject<null>();
+  mainList$ = merge(this.onInit$, this.onFilterChange$, this.onPageChange$).pipe(
+    tap(() => console.log('onInit$')),
+    switchMap(() => this.dataProvider$(1))
+  );
+  dataProvider$ = this.provider.getListProvider('clinics');
+  mode: 'map' | 'list' = 'list';
 
-    constructor(
-        private provider: DataProviderService,
-    ) {
-    }
+  constructor(
+    private provider: DataProviderService,
+  ) {
+  }
 
-    ngOnInit(): void {
-    }
+  ngOnInit(): void {
+  }
 
-    ngAfterViewInit(): void {
-        this.onInit$.next(null);
-    }
+  ngAfterViewInit(): void {
+    this.onInit$.next(null);
+  }
+
+  modeMap(): void {
+    this.mode = 'map';
+  }
+
+  modeList(): void {
+    this.mode = 'list';
+  }
 }
