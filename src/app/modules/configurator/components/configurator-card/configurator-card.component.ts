74d30fd13a6ecf793c1a11a5fd669c00d04fe6a2
exclucivelogin <alter4444@gmail.com>
2021-11-25 19:12:31 +0400

    locker and invalidator in configurator improved

--- src/app/modules/configurator/components/configurator-card/configurator-card.component.ts
+++ src/app/modules/configurator/components/configurator-card/configurator-card.component.ts
@@ -6,7 +6,9 @@ import {PersonBuilder, PersonDoctorSlot} from 'app/models/doctor.interface';
 import {PlacementBuilder, PlacementSlot} from 'app/models/placement.interface';
 import {CardSlot, ConfiguratorCardBuilder} from 'app/models/cardbuilder.interface';
 import {ConfiguratorService} from '../../configurator.service';
-import {tap} from 'rxjs/operators';
+import {map, tap} from 'rxjs/operators';
+import {Observable, combineLatest} from 'rxjs';
+import {OrderService} from '../../../../services/order.service';
 
 @Component({
     selector: 'app-configurator-card',
@@ -40,8 +42,12 @@ export class ConfiguratorCardComponent implements OnInit {
         this.url = `${environment.static}/${this.viewEnt.photo_url || 'noimage'}`;
         this.refreshSelectionState();
     }
+
+    isLocked$: Observable<boolean>;
+    isInvalid$: Observable<boolean>;
     constructor(
         private configuratorService: ConfiguratorService,
+        private orderService: OrderService,
     ) {
     }
 
@@ -59,6 +65,20 @@ export class ConfiguratorCardComponent implements OnInit {
 
     ngOnInit(): void {
         // console.log('ConfiguratorCardComponent', this);
+        this.isLocked$ = combineLatest([
+            this.configuratorService.getValidationStateTabByKey(this.tabKey),
+            this.configuratorService.getValidationStateFloorByKey(this.floorKey),
+        ]).pipe(
+            map(data => data.reduce((locked, cur) => locked || (cur?.locked ?? false), false)),
+            tap(state => console.log('isLocked state:', state, this.viewEnt.id))
+        );
+        this.isInvalid$ = combineLatest([
+            this.configuratorService.getValidationStateTabByKey(this.tabKey),
+            this.configuratorService.getValidationStateFloorByKey(this.floorKey),
+        ]).pipe(
+            map(data => data.reduce((invalid, cur) =>
+                invalid || (cur?.status === 'rich' || cur?.status === 'poor'), false)),
+        );
     }
 
 }
