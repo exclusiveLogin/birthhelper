15153f0828781bfe4ff20facd111332fae9bd237
Sergey Savin <alter4444@gmail.com>
2021-12-05 01:02:45 +0400

    backdrop filter perf fix

--- src/app/modules/dialog/dialog.animation.ts
+++ src/app/modules/dialog/dialog.animation.ts
@@ -1,8 +1,8 @@
 import {animate, animateChild, group, query, state, style, transition, trigger} from '@angular/animations';
 
 export const dialogWrapperAnimation = trigger('dialogWrapper', [
-    state('void', style({opacity: 0, backdropFilter: 'blur(0)'})),
-    state('*', style({opacity: 1, backdropFilter: 'blur(4px)'})),
+    state('void', style({opacity: 0})),
+    state('*', style({opacity: 1})),
     transition('* => *', group([
         query('@dialogState', animateChild()),
         animate('150ms'),
@@ -10,8 +10,8 @@ export const dialogWrapperAnimation = trigger('dialogWrapper', [
 ]);
 
 export const dialogAnimation = trigger('dialogState', [
-    state('void', style({transform: 'matrix(1.2, 0, 0, 0.2, 0, -400)', filter: 'blur(5px)'})),
-    state('*', style({transform: 'matrix(1, 0, 0, 1, 0, 0)', filter: 'blur(0)'})),
+    state('void', style({transform: 'matrix(1.2, 0, 0, 0.2, 0, -400)'})),
+    state('*', style({transform: 'matrix(1, 0, 0, 1, 0, 0)'})),
     transition(':leave', animate('150ms', style({transform: 'matrix(1.2, 0, 0, 0.2, 0, 400)'}))),
     transition(':enter', animate('150ms')),
 ]);
