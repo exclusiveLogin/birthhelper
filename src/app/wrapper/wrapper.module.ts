80e9e87c354b0e2214a28dde3142f03d57b74c40
Sergey Savin <alter4444@gmail.com>
2021-12-17 22:06:28 +0400

    import fix

--- src/app/wrapper/wrapper.module.ts
+++ src/app/wrapper/wrapper.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {WrapperModuleRouting} from 'app/wrapper/wrapper.module.routing';
+import {WrapperModuleRouting} from './wrapper.module.routing';
 
 
 
