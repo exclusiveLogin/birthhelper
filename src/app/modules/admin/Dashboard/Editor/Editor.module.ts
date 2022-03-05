a13c169ebd9011b682e268971230c858c452c81b
Sergei Savin <alter4444@gmail.com>
2022-03-05 16:43:57 +0400

    add autocomplete

--- src/app/modules/admin/Dashboard/Editor/Editor.module.ts
+++ src/app/modules/admin/Dashboard/Editor/Editor.module.ts
@@ -1,30 +1,33 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { EditorComponent } from './Editor.component';
-import { TableModule } from '../../table/table.module';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AutocompleteComponent } from '../../autocomplete/autocomplete.component';
-import { ImageComponent } from './components/image/image.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {EditorComponent} from './Editor.component';
+import {TableModule} from '../../table/table.module';
+import {ReactiveFormsModule} from '@angular/forms';
+import {AutocompleteComponent} from '../../autocomplete/autocomplete.component';
+import {ImageComponent} from './components/image/image.component';
 import {ToastrService} from 'ngx-toastr';
 import {MapComponent} from '@modules/admin/Dashboard/Editor/components/map/map.component';
+import {NgxAutocompleteModule} from 'ngx-angular-autocomplete';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    TableModule,
-    ReactiveFormsModule,
-  ],
-  declarations: [
-    EditorComponent,
-    AutocompleteComponent,
-    ImageComponent,
-    MapComponent,
-  ],
-  exports: [
-    EditorComponent
-  ],
-  providers: [
-    ToastrService,
-  ]
+    imports: [
+        CommonModule,
+        TableModule,
+        ReactiveFormsModule,
+        NgxAutocompleteModule,
+    ],
+    declarations: [
+        EditorComponent,
+        AutocompleteComponent,
+        ImageComponent,
+        MapComponent,
+    ],
+    exports: [
+        EditorComponent
+    ],
+    providers: [
+        ToastrService,
+    ]
 })
-export class EditorModule { }
+export class EditorModule {
+}
