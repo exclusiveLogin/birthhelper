28f0ce41ecdd08596d4d3a54de834820c8f87274
Sergei Savin <alter4444@gmail.com>
2022-03-21 01:32:58 +0400

    slider wip

--- src/app/main/components/ad-slider/ad-slider.component.ts
+++ src/app/main/components/ad-slider/ad-slider.component.ts
@@ -1,16 +1,40 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ElementRef, OnDestroy, OnInit} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+
+export interface SliderSettings {
+    id: string;
+    imageFineName: string;
+    backgroundColor: string;
+    price?: string;
+    title?: string;
+    accentTitle?: string;
+    description?: string;
+    link?: string[];
+}
 
 @Component({
     selector: 'app-ad-slider',
     templateUrl: './ad-slider.component.html',
-    styleUrls: ['./ad-slider.component.scss']
+    styleUrls: ['./ad-slider.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AdSliderComponent implements OnInit {
+export class AdSliderComponent implements OnInit, OnDestroy {
 
-    constructor() {
+    constructor(private el: ElementRef) {
     }
+
+    showPicture$ = new BehaviorSubject(false);
 
     ngOnInit(): void {
+        const obs = new ResizeObserver((entries) => {
+            console.log('ResizeObserver:', entries);
+            const ent = entries?.[0] ?? null;
+            if (!ent) { return; }
+            this.showPicture$.next(ent.contentRect.width > 640);
+        });
+        obs.observe(this.el.nativeElement);
     }
 
+    ngOnDestroy() {
+    }
 }
