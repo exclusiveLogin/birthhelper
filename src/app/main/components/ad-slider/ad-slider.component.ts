973fce99fbd354e4521039db16d93a74df690d91
Sergei Savin <alter4444@gmail.com>
2022-03-21 17:45:24 +0400

    slider wip demo

--- src/app/main/components/ad-slider/ad-slider.component.ts
+++ src/app/main/components/ad-slider/ad-slider.component.ts
@@ -1,10 +1,11 @@
-import {ChangeDetectionStrategy, Component, ElementRef, OnDestroy, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ElementRef, Input, OnDestroy, OnInit} from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
 
 export interface SliderSettings {
     id: string;
-    imageFineName: string;
+    imageFileName: string;
     backgroundColor: string;
+    backgroundColorOpacity?: string;
     backgroundPicture?: string;
     price?: string;
     priceColor?: string;
@@ -17,6 +18,10 @@ export interface SliderSettings {
     link?: string[];
 }
 
+interface Style {
+    [key: string]: string;
+}
+
 @Component({
     selector: 'app-ad-slider',
     templateUrl: './ad-slider.component.html',
@@ -25,17 +30,74 @@ export interface SliderSettings {
 })
 export class AdSliderComponent implements OnInit, OnDestroy {
 
-    constructor(private el: ElementRef) {
-    }
+    wrapperStyle: Style = {};
+    pictureStyle: Style = {};
+    titleStyle: Style = {};
+    accentStyle: Style = {};
+    descriptionStyle: Style = {};
+    priceStyle: Style = {};
+    title: string;
+    accentTitle: string;
+    description: string;
+    price: string;
+    link: string[];
 
     showPicture$ = new BehaviorSubject(false);
+
+    @Input() set settings(value: SliderSettings) {
+        if ( value.title ) {
+            this.title = value.title;
+        }
+        if ( value.link ) {
+            this.link = value.link;
+        }
+        if ( value.accentTitle ) {
+            this.accentTitle = value.accentTitle;
+        }
+        if ( value.description ) {
+            this.description = value.description;
+        }
+        if ( value.price ) {
+            this.price = value.price;
+        }
+        if ( value.backgroundColor ) {
+            this.wrapperStyle.backgroundColor = value.backgroundColor + value.backgroundColorOpacity ?? 'ff';
+        }
+
+        if ( value.backgroundPicture ) {
+            this.wrapperStyle.backgroundImage = `url("${value.backgroundPicture}")`;
+        }
+
+        if ( value.imageFileName ) {
+            this.pictureStyle.backgroundImage = `url("/img/slider/${value.imageFileName}")`;
+        }
+
+        if ( value.titleColor ) {
+            this.titleStyle.color = value.titleColor;
+        }
+
+        if ( value.accentTitleColor ) {
+            this.accentStyle.color = value.accentTitleColor;
+        }
+
+        if ( value.descriptionColor ) {
+            this.descriptionStyle.color = value.descriptionColor;
+        }
+
+        if ( value.priceColor ) {
+            this.priceStyle.color = value.priceColor;
+        }
+    }
+
+    constructor(private el: ElementRef) {
+    }
 
     ngOnInit(): void {
         const obs = new ResizeObserver((entries) => {
             console.log('ResizeObserver:', entries);
             const ent = entries?.[0] ?? null;
             if (!ent) { return; }
-            this.showPicture$.next(ent.contentRect.width > 640);
+            this.showPicture$.next(ent.contentRect.width > 640 && !!this.pictureStyle?.backgroundImage);
         });
         obs.observe(this.el.nativeElement);
     }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,6 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {AuthService} from '../modules/auth-module/auth.service';
+import {SliderSettings} from './components/ad-slider/ad-slider.component';
 
 @Component({
     selector: 'app-main',
@@ -10,6 +11,34 @@ import {AuthService} from '../modules/auth-module/auth.service';
 export class MainComponent implements OnInit {
 
     path = location.pathname;
+    banner1_test: SliderSettings = {
+        id: '1',
+        title: 'Тестовый баннер 1',
+        accentTitle: 'Какое то акцентное описание баннера или услуги',
+        description: 'Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.',
+        price: '15000р',
+        imageFileName: 'doctor1.png',
+        backgroundColor: '#ffbcfc',
+        backgroundColorOpacity: 'ee',
+        accentTitleColor: '#ff2626',
+        priceColor: '#ff4eb8',
+        link: ['/'],
+    };
+    banner2_test: SliderSettings = {
+        id: '2',
+        title: 'Тестовый баннер 2',
+        accentTitle: 'Какое то акцентное описание баннера или услуги',
+        description: 'Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.',
+        price: '35000р',
+        imageFileName: 'doctor2.png',
+        backgroundColor: '#10599e',
+        backgroundColorOpacity: 'ee',
+        accentTitleColor: '#c6e6ff',
+        titleColor: '#a5d8fd',
+        descriptionColor: '#fde4a5',
+        priceColor: '#5ce7ff',
+        link: ['/'],
+    };
 
     constructor( public auth: AuthService) {
     }
