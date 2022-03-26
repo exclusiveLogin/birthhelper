05b5e28def397f316a244d0f1d5b64c38b98dbc7
Sergei Savin <alter4444@gmail.com>
2022-03-26 15:47:09 +0400

    consultations en in configurator

--- src/app/modules/configurator/configurator.module.routing.ts
+++ src/app/modules/configurator/configurator.module.routing.ts
@@ -3,19 +3,30 @@ import {NotFoundComponent} from '../../NotFound/NotFound.component';
 import {ConfiguratorComponent} from './configurator.component';
 import {ConfiguratorGuard} from './configurator.guard';
 
-const routes: Routes = [{
-    path: 'clinic',
-    data: {
-        section: 'clinic',
-        entity_key: 'ent_clinics'
-    },
-    children: [
-        { path: '', redirectTo: '/system/search/clinics', pathMatch: 'full'},
-        { path: ':id', component: ConfiguratorComponent, canActivate: [ConfiguratorGuard]},
-    ],
-}, {
-    path: '**', component: NotFoundComponent,
-}];
+const routes: Routes = [
+    {
+        path: 'clinic',
+        data: {
+            section: 'clinic',
+            entity_key: 'ent_clinics'
+        },
+        children: [
+            {path: '', redirectTo: '/system/search/clinics', pathMatch: 'full'},
+            {path: ':id', component: ConfiguratorComponent, canActivate: [ConfiguratorGuard]},
+        ],
+    }, {
+        path: 'consultation',
+        data: {
+            section: 'consultation',
+            entity_key: 'ent_consultations'
+        },
+        children: [
+            {path: '', redirectTo: '/system/search/consultations', pathMatch: 'full'},
+            {path: ':id', component: ConfiguratorComponent, canActivate: [ConfiguratorGuard]},
+        ],
+    }, {
+        path: '**', component: NotFoundComponent,
+    }];
 
 
 // tslint:disable-next-line: variable-name
